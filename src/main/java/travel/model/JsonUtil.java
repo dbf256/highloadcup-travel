a263d82e1e6cd0172eb249abbabfe9e16f586392
dbf <dbf@eb7d2e96-c453-4c4b-9b9b-4dc0903f1c9d>
2017-08-27 09:13:12 +0000

    More new generation, remove intermediate object in json parser

--- src/main/java/travel/model/JsonUtil.java
+++ src/main/java/travel/model/JsonUtil.java
@@ -3,7 +3,6 @@ package travel.model;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import org.apache.commons.lang3.StringUtils;
 
 public class JsonUtil {
     public static String getAsString(JsonObject object, String field) {
@@ -28,24 +27,12 @@ public class JsonUtil {
         }
     }
 
-    private static boolean isNumeric(String s) {
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if ((i == 0 && c == '-') || (c >= '0' && c <= '9')) {
-                continue;
-            }
-            return false;
-        }
-        return true;
-    }
-
     public static int getAsIntegerPrimitive(JsonObject object, String field) {
         JsonElement element = object.get(field);
         if (element != null && !element.isJsonNull()) {
-            String strElem = element.getAsString();
-            if (isNumeric(strElem)) {
+            try {
                 return element.getAsInt();
-            } else {
+            } catch (ClassCastException e) {
                 throw new IllegalArgumentException();
             }
         } else if (element != null && element.isJsonNull()) {
--- src/main/resources/docker/start.sh
+++ src/main/resources/docker/start.sh
@@ -5,4 +5,4 @@ unzip /tmp/data/data.zip -d /travel/data
 cp /tmp/data/options.txt /travel/data
 export DATA_PATH=/travel/data
 cd /travel/app
-java -Xms3500m -Xmx3500m -XX:+PrintGCTimeStamps -XX:+PrintGC -cp travel-1.0.jar travel.Main
+java -Xms3500m -Xmx3500m -XX:MaxNewSize=1700m -XX:+PrintGCTimeStamps -XX:+PrintGC -cp travel-1.0.jar travel.Main
